52983e9e5012e7b83354e5a3fe829b7a81b329c3
Iyatan Atchoro <iyatan.atchoro@mail.mcgill.ca>
2020-04-14 02:47:42 -0400

    Updates--

--- src/Mainfile.java
+++ src/Mainfile.java
@@ -20,25 +20,38 @@ public class Mainfile {
 
 	public static void main(String[] args) throws SQLException {
 
-        boolean notDone = true;
-        System.out.println("Please enter a number ");
+        boolean Active = true;
+        System.out.println("Please enter a code to execcute");
+        System.out.println("At any point press 911 for help");
 
         input = new Scanner(System.in);
 
-        while (notDone) {
-        	   int i = input.nextInt();
+        while (Active) {
+        	   int i = (int) input.nextInt();
                switch (i) {
+                   case 911:
+                	   System.out.println("**** Press 0 to quit *****");
+                	   System.out.println("**** Press 1 to show guests ***");
+                	   System.out.println("**** Press 2 to increase the salaries of the SPA therapists whose rating is higher than 4 by $200 ***** ");
+                	   break;
                    case 0:
-                       System.out.println("We are sad to see you go :(");
-                       notDone = false;
+                       System.out.println("See you next time");
+                       Active = false;
                        System.exit(0);
                        break;
                    case 1:
-                       SelectingBooks();
+                       SelectingGuess();
                        break;
+                   case 2:
+                	   UpdateSalary();
+                	   break;
+                   case 3:
+                	   UpdateSparoomStatus();
+                	   break;
+                	   
                    
                    default:
-                       System.out.println("Unknown Selection");
+                       System.out.println("Sorry we dont know this command");
                        break;
         	
         }
@@ -46,7 +59,7 @@ public class Mainfile {
 	}
 }
 	//quering a table
-	private static void SelectingBooks() throws SQLException {
+	private static void SelectingGuess() throws SQLException {
 		 int sqlCode = 0; // Variable to hold SQLCODE
 	        String sqlState = "00000"; // Variable to hold SQLSTATE
 
@@ -62,14 +75,17 @@ public class Mainfile {
 	        
 	        // Querying a table
 	        try {
-	            String querySQL = "SELECT * from books";
+	            String querySQL = "SELECT * from guest";
 	            // System.out.println(querySQL);
 	            java.sql.ResultSet rs = statement.executeQuery(querySQL);
 	            while (rs.next()) {
-	                int id = rs.getInt(1);
+	                String fam = rs.getString(1);
 	                String name = rs.getString(2);
-	                System.out.print("book Id :  " + id);
-	                System.out.println("  Book name:  " + name);
+	                String email = rs.getString(3);
+	                System.out.print("  FAMILY NAME : " + fam);
+	                System.out.print("  FIRST NAME:   " + name);
+	                System.out.println("  EMAIL:      " + email);
+	                System.out.println("********************************************************************************");
 	            }
 	            System.out.println("DONE");
 	        } catch (SQLException e) {
@@ -82,6 +98,72 @@ public class Mainfile {
 	        statement.close();
 	        con.close();
 	}
+	
+	//The query is aimed to increase SPA therapists' salary if he/she has a rating higher or equal to 4.
+
+	
+	private static void UpdateSalary() throws SQLException {
+		int error =0;
+		String state ="00000";
+		
+		try {
+			DriverManager.registerDriver(new org.postgresql.Driver());
+			
+		}catch(Exception cnfe) {
+			System.out.println("We can not find the class");
+			
+		}
+		con = DriverManager.getConnection(url, usernamestring, passwordstring);
+		Statement stm = con.createStatement();
+		
+		try {
+			String updateSQL = "SELECT employeeID, salary, rating FROM SPATherapist;\n" + 
+					"UPDATE SPATherapist \n" + 
+					"	SET salary = salary + 200\n" + 
+					"	WHERE rating >= 4;\n" + 
+					"SELECT employeeID, salary, rating FROM SPATherapist;";
+			stm.executeUpdate(updateSQL);
+			System.out.println("SALARIES UPDATED");
+					
+		}catch(SQLException e) {
+			error = e.getErrorCode();
+			state = e.getSQLState();
+		}
+
+	}
+	
+	// make some rooms status uncompleted 
+		private static void UpdateSparoomStatus() throws SQLException {
+			int error =0;
+			String state ="00000";
+			
+			try {
+				DriverManager.registerDriver(new org.postgresql.Driver());
+				
+			}catch(Exception cnfe) {
+				System.out.println("We can not find the class");
+				
+			}
+			con = DriverManager.getConnection(url, usernamestring, passwordstring);
+			Statement stm = con.createStatement();
+			
+			try {
+				String updateSQL = "UPDATE sparoom SET status = 'under construction' where roomnumber >17 ;\n" + 
+						"";
+				stm.executeUpdate(updateSQL);
+				System.out.println("STATUS UPDATED");
+						
+			}catch(SQLException e) {
+				error = e.getErrorCode();
+				state = e.getSQLState();
+	            System.out.println("Code: " + error + "  sqlState: " + state);
+
+			}
+			stm.close();
+			con.close();
+			
+			
+		}
         
 
 }
